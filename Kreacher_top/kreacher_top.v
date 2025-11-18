28e1d6c3162f4a403532b3d8b04f778526d15b64
Jhon Steven Pinto Hernandez <jhon_steven.pinto_hernandez@mailbox.tu-dresden.de>
2025-11-18 18:59:14 +0100

    Added top file for kreacher

---
+++ Kreacher_top/kreacher_top.v
@@ -0,0 +1,56 @@
+module kreachcer_top #(parameter XLEN = 32)(
+    //Global
+    input clk,
+    input reset,
+
+    //TEMPORARY (ONLY FOR TB PURPOSES)
+    input [1:0]      sel_next_PC,
+
+    input [2:0]      imm_type,
+    input            regfile_we,
+
+    input [1:0]      sel_opa,
+    input [1:0]      sel_opb,
+    input [4:0]      sel_op,
+
+    input            mem_wr_en,
+    input [2:0]      val_rd_type,
+    input [2:0]      val_wr_type,
+    
+    input [2:0]      sel_writeback
+);
+
+// ------------------------------------------------ Buses
+wire [XLEN-1:0] EIB,  //External Instruction Bus
+wire [XLEN-1:0] EIAB, //External Instruction Address Bus
+
+wire [XLEN-1:0] EMAB,            //External Memory Address Bus
+wire            EMCB,            //External Memory Control Bus
+wire [XLEN-1:0] EMDB_out,        //External Memory Data Bus, output for the core, input for the external memory
+wire [XLEN-1:0] EMDB_in,         //External Memory Data Bus, input for the core, output for the external memory
+
+core #(.XLEN(XLEN)) core_inst (
+    .clk(clk), 
+    .reset(reset),
+    .EIB(EIB),
+    .EIAB(EIAB),
+    .EMAB(EMAB),
+    .EMCB(EMCB),
+    .EMDB_out(EMDB_out),
+    .EMDB_in(EMDB_in),
+
+    //TEMPORARY (ONLY FOR TB PURPOSES)
+    .sel_next_PC(sel_next_PC),
+    .imm_type(imm_type),
+    .regfile_we(regfile_we),
+    .sel_opa(sel_opa),
+    .sel_opb(sel_opb),
+    .sel_op(sel_op),
+    .mem_wr_en(mem_wr_en),
+    .val_rd_type(val_rd_type),
+    .val_wr_type(val_wr_type),
+    .sel_writeback(sel_writeback)
+);
+
+
+endmodule
