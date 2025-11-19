24c002e0424134862ad10c5988f0677eb2b03e57
sanjai-commits <sanjai.palanisamy171@gmail.com>
2025-11-19 16:29:04 +0100

    control_v3

--- Core/2_ID/control.v
+++ Core/2_ID/control.v
@@ -29,9 +29,9 @@ module control(
 );
 always@(opcode or funct3 or funct7)begin //combinational circuit
     case(opcode)
-        SHIFT_ARITHMETIC:begin
-            case(func3)
-                ADDI:begin
+        `SHIFT_ARITHMETIC_I:begin
+            case(funct3)
+                `ADDI:begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -43,7 +43,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                SLTI:begin
+                `SLTI:begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -55,7 +55,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                SLTIU: begin
+                `SLTIU: begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -67,7 +67,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                ANDI:begin
+                `ANDI:begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -79,7 +79,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                XORI:begin
+                `XORI:begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -91,7 +91,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                ORI:begin
+                `ORI:begin
                      sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -103,7 +103,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                SLLI:begin//check
+                `SLLI:begin//check
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -115,7 +115,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
                     val_rd_type = `MEM_NOT_USED;
                     sel_writeback = `WBACK_ALU_OUT;
                 end
-                SRLI_SRAI:begin
+                `SRLI_SRAI:begin
                     sel_next_pc = `NEXT_PC_4;
                     regfile_we = `ENABLE;
                     imm_type = `I_IMMEDIATE;
@@ -130,7 +130,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
             endcase
         end
 
-        LUI: begin
+        `LUI: begin
             sel_next_pc = `NEXT_PC_4;
             regfile_we = `DISABLE;
             imm_type = `U_IMMEDIATE;
@@ -143,7 +143,7 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
             sel_writeback = `WBACK_ALU_OUT;  
         end
 
-        AUIPC:begin
+        `AUIPC:begin
             sel_next_pc = `NEXT_PC_4;
             regfile_we = `DISABLE;
             imm_type = `U_IMMEDIATE;
@@ -156,7 +156,25 @@ always@(opcode or funct3 or funct7)begin //combinational circuit
             sel_writeback = `WBACK_ALU_OUT;  
         end
 
-        ADD:begin
+        `SHIFT_ARITHMETIC:begin
+            case(funct3)
+                `AND:begin
+                    sel_next_pc = `NEXT_PC_4;
+                    regfile_we = `ENABLE;
+                    imm_type = ;
+                    sel_opa = ;
+                    sel_opb = ;
+                    sel_op = ;
+                    mem_wr_en = ;
+                    val_wr_type = ;
+                    val_rd_type = ;
+                    sel_writeback = ;
+                end
+            endcase
+        end
+        
+
+        default:begin
             sel_next_pc = ;
             regfile_we = ;
             imm_type = ;
--- Core/CORE_CONSTANTS.vh
+++ Core/CORE_CONSTANTS.vh
@@ -1,77 +1,94 @@
-// Execution Unit constants
-`ifndef EXEC_CONSTANTS_VH
-`define EXEC_CONSTANTS_VH
+    // Execution Unit constants
+    `ifndef EXEC_CONSTANTS_VH
+    `define EXEC_CONSTANTS_VH
 
-// ---- OPCODE CONSTANTS ----
-`define SHIFT_ARITHMETIC 7'b0010011 
-`define LUI 7'b0110111
-`define AUIPC 7'b0010111
+    // ---- OPCODE CONSTANTS ----
+    `define SHIFT_ARITHMETIC_I 7'b0010011 
+    `define LUI 7'b0110111
+    `define AUIPC 7'b0010111
+    `define SHIFT_ARITHMETIC 7'b0110011
 
+    // ---- func3 ----SHIFT_ARITHMETIC
+    `define ARITHMETIC 3'b000
+    `define SLL 3'b001
+    `define SLT 3'b010
+    `define SLTU 3'b011
+    `define XOR  3'b100
+    `define SHIFT 3'b101
+    `define OR 3'b110
+    `define AND 3'b111
 
+    // ---- func7 ---- SHIFT_ARITHMETIC-SHIFT
+    `define SRL 7'b0000000 
+    `define SRA 7'b0100000
 
-// ---- func3 ----SHIFT_ARITHMETIC
-`define ADDI 3'b000
-`define SLLI 3'b001
-`define SLTI 3'b010
-`define SLTIU 3'b011
-`define ANDI 3'b100
-`define SRLI_SRAI 3'b101
-`define XORI 3'b110
-`define  ORI 3'b111 
+    // ---- func7 ---- SHIFT_ARITHMETIC - ARITHMETIC
+    `define ADD 7'b0000000 
+    `define SUB 7'b0100000
 
-// ---- func7 ----
+    // ---- func3 ----SHIFT_ARITHMETIC_I
+    `define ADDI 3'b000
+    `define SLLI 3'b001
+    `define SLTI 3'b010
+    `define SLTIU 3'b011
+    `define ANDI 3'b100
+    `define SRLI_SRAI 3'b101
+    `define XORI 3'b110
+    `define  ORI 3'b111 
 
-
-// ---- ALU CONSTANTS ----
-`define ALU_ADD         5'b00000
-`define ALU_SUB         5'b00001
-`define ALU_SLT         5'b00010
-`define ALU_SLTU        5'b00011
-`define ALU_AND         5'b00100
-`define ALU_OR          5'b00101
-`define ALU_XOR         5'b00110
-`define ALU_SLL         5'b00111
-`define ALU_SRL         5'b01000
-`define ALU_SRA         5'b01001
-`define ALU_FORWARD_B   5'b01010
-
-// --- CONTROL CONSTANTS ----
-
-//sel_next_pc
-`define NEXT_PC_4         2'b00
-`define NEXT_PC_ALU_OUT   2'b01
-
-//sel_opa
-`define OPA_PC      2'b00
-`define OPA_RS1     2'b01
-
-//sel_opb
-`define OPB_IMM     2'b00
-`define OPB_RS2     2'b01
-
-//sel_writeback
-`define WBACK_ALU_OUT    3'b000
-`define WBACK_PC_4       3'b001
-`define WBACK_EMDB       3'b010
-
-//val_rd_type, val_wr_type (mem stage)
-`define FORWARD_INPUT   3'b000
-`define SIGN_EXTEND_16  3'b001
-`define SIGN_EXTEND_8   3'b010
-`define ZERO_EXTEND_16  3'b011
-`define ZERO_EXTEND_8   3'b100
-`define MEM_NOT_USED 3'b101
-
-//imm_type
-`define I_IMMEDIATE 3'b000
-`define S_IMMEDIATE 3'b001
-`define B_IMMEDIATE 3'b010
-`define U_IMMEDIATE 3'b011
-`define J_IMMEDIATE 3'b100
-// `define I_IMMEDIATE_shamt 3'b101
+    // ---- func7 ----
 
 
-`define ENABLE 1'b1
-`define DISABLE 1'b0
+    // ---- ALU CONSTANTS ----
+    `define ALU_ADD         5'b00000
+    `define ALU_SUB         5'b00001
+    `define ALU_SLT         5'b00010
+    `define ALU_SLTU        5'b00011
+    `define ALU_AND         5'b00100
+    `define ALU_OR          5'b00101
+    `define ALU_XOR         5'b00110
+    `define ALU_SLL         5'b00111
+    `define ALU_SRL         5'b01000
+    `define ALU_SRA         5'b01001
+    `define ALU_FORWARD_B   5'b01010
 
-`endif
+    // --- CONTROL CONSTANTS ----
+
+    //sel_next_pc
+    `define NEXT_PC_4         2'b00
+    `define NEXT_PC_ALU_OUT   2'b01
+
+    //sel_opa
+    `define OPA_PC      2'b00
+    `define OPA_RS1     2'b01
+
+    //sel_opb
+    `define OPB_IMM     2'b00
+    `define OPB_RS2     2'b01
+
+    //sel_writeback
+    `define WBACK_ALU_OUT    3'b000
+    `define WBACK_PC_4       3'b001
+    `define WBACK_EMDB       3'b010
+
+    //val_rd_type, val_wr_type (mem stage)
+    `define FORWARD_INPUT   3'b000
+    `define SIGN_EXTEND_16  3'b001
+    `define SIGN_EXTEND_8   3'b010
+    `define ZERO_EXTEND_16  3'b011
+    `define ZERO_EXTEND_8   3'b100
+    `define MEM_NOT_USED 3'b101
+
+    //imm_type
+    `define I_IMMEDIATE 3'b000
+    `define S_IMMEDIATE 3'b001
+    `define B_IMMEDIATE 3'b010
+    `define U_IMMEDIATE 3'b011
+    `define J_IMMEDIATE 3'b100
+    // `define I_IMMEDIATE_shamt 3'b101
+
+
+    `define ENABLE 1'b1
+    `define DISABLE 1'b0
+
+    `endif
