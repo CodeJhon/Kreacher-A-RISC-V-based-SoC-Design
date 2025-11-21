6e926b726bed028542d0fbb87154a3a1d170cabd
Jhon Steven Pinto Hernandez <jhon_steven.pinto_hernandez@mailbox.tu-dresden.de>
2025-11-21 23:50:01 +0100

    changed tb for making it more comfortable to use

--- Kreacher_top/tb/kreacher_top_tb.v
+++ Kreacher_top/tb/kreacher_top_tb.v
@@ -6,6 +6,7 @@ module kreacher_top_tb;
 
 // Parameters
 localparam XLEN = 32;
+localparam RUN_NUMBER_OF_INSTRUCTIONS = 9;
 
 // Signals
 reg clk;
@@ -53,75 +54,26 @@ initial begin
     reset = 0;
 end
 
+
+localparam CYCLES = 10*(RUN_NUMBER_OF_INSTRUCTIONS -1);
+
 // Stimulus
 initial begin
     
     //Wait for reset
     @(negedge reset);
-
     //First fetch
-    sel_next_PC = `NEXT_PC_4;
     #10;
-
-    // Instruction #1: lui x1, 0x5
-    sel_next_PC = `NEXT_PC_4;
-
-    imm_type    = `U_IMMEDIATE;
-    regfile_we  = 1;
-
-    //sel_opa     = `OPA_RS1;
-    sel_opb     = `OPB_IMM;
-    sel_op      = `ALU_FORWARD_B;
     
-    mem_wr_en   = 0;
-    val_rd_type = 3'b111;
-    val_wr_type = 3'b111;
-
-    sel_writeback = `WBACK_ALU_OUT;
-
+    
     // Wait some cycles
-    #10;
-
-    // Instruction #2: addi x12 x0, 0x200
-    sel_next_PC = `NEXT_PC_4;
-
-    imm_type    = `I_IMMEDIATE;
-    regfile_we  = 1;
-
-    sel_opa     = `OPA_RS1;
-    sel_opb     = `OPB_IMM;
-    sel_op      = `ALU_ADD;
-    
-    mem_wr_en   = 0;
-    val_rd_type = 3'b111;
-    val_wr_type = 3'b111;
-
-    sel_writeback = `WBACK_ALU_OUT;
-
-    // Wait some cycles
-    #10;
-
-    // Instruction #3: sw x1 0(x12)
-    sel_next_PC = `NEXT_PC_4;
-
-    imm_type    = `S_IMMEDIATE;
-    regfile_we  = 0;
-
-    sel_opa     = `OPA_RS1;
-    sel_opb     = `OPB_IMM;
-    sel_op      = `ALU_ADD;
-    
-    mem_wr_en   = 1;
-    val_rd_type = 3'b111;
-    val_wr_type = `FORWARD_INPUT;
-
-    //sel_writeback = `WBACK_ALU_OUT;
+    #CYCLES;
 
     //Wait latency cycles
     #30;
 
     // Wait final cycles
-    #9;
+    #7;
 
     $display("Writing DMEM to file...");
     $writememh("DMEM_result.mem", DUT.memories_top_inst.DMEM.memory); 
